b65d1779d023a15c38662ebe109d3d0bb60cc602
Vibhatha Lakmal Abeykoon <vibhatha@users.noreply.github.com>
2018-05-29 00:19:32 -0400

    new change for learning

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,5 +4,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Travis Build Sample");
+        System.out.println("Travis Build Sample1");        
     }
 }
